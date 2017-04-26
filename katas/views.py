e354f65617ce5b6f154ca46f084c2ce152f99b15
blackmantram <misantropee@gmail.com>
2017-04-25 20:06:21 -0500

    removed unused references

--- katas/views.py
+++ katas/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from katas.models import Kata
 from katas.serializers import KataSerializer
 from rest_framework import generics
